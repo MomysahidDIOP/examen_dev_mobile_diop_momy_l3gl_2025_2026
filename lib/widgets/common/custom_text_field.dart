5923742b962a1f69458daf2a8b18bfe87e2174d3
Momy Diop <momydiop234@gmail.com>
2026-03-02 19:20:23 +0000

    feat: ajout du widget CustomTextField

---
+++ lib/widgets/common/custom_text_field.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget
+{
+  final TextEditingController controller;
+  final String label;
+  final String hint;
+  final IconData prefixIcon;
+  final bool isPassword;
+  final TextInputType keyboardType;
+  final String? Function(String?)? validator;
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.label,
+    required this.hint,
+    required this.prefixIcon,
+    this.isPassword = false,
+    this.keyboardType = TextInputType.text,
+    this.validator,
+  });
+
+  @override
+
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: isPassword,
+      keyboardType: keyboardType,
+      validator: validator,
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hint,
+        prefixIcon: Icon(prefixIcon),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        // Etat  quand le champ n'est pas selectionne
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: Colors.grey.shade300),
+        ),
+        // Etat quand on clique sur le champ
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: Theme.of(context).primaryColor, width: 2),
+        ),
+      ),
+    );
+  }
+
+
+}
