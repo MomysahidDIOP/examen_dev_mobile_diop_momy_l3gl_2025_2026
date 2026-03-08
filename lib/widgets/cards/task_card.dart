3f5e1857bc68eb4eb00a9c088f8457be494ca843
Momy Diop <momydiop234@gmail.com>
2026-03-08 17:03:20 +0000

    fix: correction TaskCard avec enums TaskStatus/TaskPriority et AppColors

--- lib/widgets/cards/task_card.dart
+++ lib/widgets/cards/task_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../models/Task.dart';
-
+import '../../core/constants/app_colors.dart';
+import '../../core/constants/app_strings.dart';
 
 class TaskCard extends StatelessWidget {
   final Task task;
@@ -14,26 +15,38 @@ class TaskCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Déterminer la couleur selon le statut (Exigence Page 8)
+
+
     Color couleurStatut;
+    String labelStatut;
     switch (task.status) {
-      case 'todo':
-        couleurStatut = Colors.orange;
+      case TaskStatus.todo:
+        couleurStatut = AppColors.statusTodo;
+        labelStatut = AppStrings.statusTodo;
         break;
-      case 'inProgress':
-        couleurStatut = Colors.blue;
+      case TaskStatus.inProgress:
+        couleurStatut = AppColors.statusInProgress;
+        labelStatut = AppStrings.statusInProgress;
         break;
-      case 'done':
-        couleurStatut = Colors.green;
+      case TaskStatus.done:
+        couleurStatut = AppColors.statusDone;
+        labelStatut = AppStrings.statusDone;
         break;
-      default:
-        couleurStatut = Colors.grey;
     }
 
-    // Déterminer la couleur selon la priorité
-    Color couleurPriorite = task.priority == 'high'
-        ? Colors.red
-        : (task.priority == 'medium' ? Colors.orange : Colors.green);
+    //  enums au lieu de Strings
+    Color couleurPriorite;
+    switch (task.priority) {
+      case TaskPriority.high:
+        couleurPriorite = AppColors.priorityHigh;
+        break;
+      case TaskPriority.medium:
+        couleurPriorite = AppColors.priorityMedium;
+        break;
+      case TaskPriority.low:
+        couleurPriorite = AppColors.priorityLow;
+        break;
+    }
 
     return Card(
       margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
@@ -47,17 +60,22 @@ class TaskCard extends StatelessWidget {
         subtitle: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(task.description, maxLines: 1, overflow: TextOverflow.ellipsis),
+            Text(
+              task.description,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
             const SizedBox(height: 4),
-            // Badge de statut (Exigence Page 8)
+
+            // Badge de statut avec la couleur AppColors
             Container(
               padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
               decoration: BoxDecoration(
-                color: couleurStatut.withOpacity(0.1),
+                color: couleurStatut.withOpacity(0.15),
                 borderRadius: BorderRadius.circular(4),
               ),
               child: Text(
-                task.status.toUpperCase(),
+                labelStatut,
                 style: TextStyle(
                   color: couleurStatut,
                   fontSize: 10,
@@ -65,9 +83,22 @@ class TaskCard extends StatelessWidget {
                 ),
               ),
             ),
+
+            // Date d'échéance si elle existe
+            if (task.dueDate != null) ...[
+              const SizedBox(height: 4),
+              Text(
+                'Échéance : ${task.dueDate!.day}/${task.dueDate!.month}/${task.dueDate!.year}',
+                style: const TextStyle(
+                  fontSize: 11,
+                  color: AppColors.textSecondary,
+                ),
+              ),
+            ],
           ],
         ),
-        // Indicateur de priorité
+
+        // Indicateur de priorité avec AppColors
         trailing: Icon(Icons.flag, color: couleurPriorite),
       ),
     );
